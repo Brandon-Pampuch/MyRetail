5080f7d61cc8819e672ded01bac93589ea0ecfba
Brandon_Pampuch <brandonpampuch@gmail.com>
2020-02-03 19:27:47 -0600

    add ability to update price in db

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const Product = require('./models/product')
 const express = require("express");
-const mongoose = require("mongoose")
 const axios = require("axios")
 const bodyParser = require('body-parser')
 const server = express();
@@ -11,7 +10,7 @@ server.use(bodyParser.json());
 server.get("/", (req, res) => {
     res.send("connected")
 })
-// post a new product
+// post a new products price
 server.post("/products", (req, res) => {
 
     const product = new Product({
@@ -30,7 +29,7 @@ server.post("/products", (req, res) => {
         createdProduct: product
     })
 })
-// retrieve product by id
+// retrieve product and it's price by id
 server.get("/products/:id", (req, res) => {
     const id = req.params.id
     axios.get(`https://redsky.target.com/v2/pdp/tcin/${id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics`)
@@ -41,7 +40,6 @@ server.get("/products/:id", (req, res) => {
                     id: parseInt(product._id),
                     name: title,
                     current_price: product.current_price
-
                 })
             })
                 .catch(err => {
@@ -52,11 +50,16 @@ server.get("/products/:id", (req, res) => {
         .catch(error => {
             console.log(error);
         });
-
 })
+// update product value in database
 
-server.put("/products", (req, res) => {
-
+server.put("/products/:id", (req, res) => {
+    Product.findByIdAndUpdate({ _id: req.params.id }, req.body)
+        .then(() => {
+            Product.findOne({ _id: req.params.id }).then(updatedPrice => {
+                res.send(updatedPrice)
+            })
+        })
 })
 
 module.exports = server
