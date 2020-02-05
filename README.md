e26ea6aa6e027f5c626c563b0e2efd7ff7516108
Brandon_Pampuch <brandonpampuch@gmail.com>
2020-02-04 20:07:17 -0600

    add nodemon script + improve error handling in product router

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # myRetail
 Retail search
+
--- routers/products-router.js
+++ routers/products-router.js
@@ -2,62 +2,67 @@ const axios = require('axios')
 const Product = require('../models/product')
 const router = require('express').Router();
 
+// POST  /products
+router.post("/", async (req, res) => {
 
-router.post("/", (req, res) => {
+    try {
+        const product = new Product({
+            _id: req.body.id,
+            current_price: {
+                value: req.body.value,
+                currency_code: req.body.currency_code
+            }
+        })
+        const result = await product.save()
+        res.status(201).json({
+            message: "product was created",
+            createdProduct: result
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: "internal server error"
+        })
+    }
+})
 
-    const product = new Product({
-        _id: req.body.id,
-        current_price: {
-            value: req.body.value,
-            currency_code: req.body.currency_code
+// GET   products/:id
+router.get("/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id
+        const apiProduct = await axios.get(`https://redsky.target.com/v2/pdp/tcin/${id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics`)
+        const title = apiProduct.data.product.item.product_description.title;
+        const foundProduct = await Product.findById(id)
+        res.status(200).json({
+            id: parseInt(foundProduct._id),
+            name: title,
+            current_price: foundProduct.current_price
+        })
+    } catch (err) {
+        res.status(404).json({
+            message: "product not found"
+        })
+    }
+})
+
+//PUT   /products/:id
+router.put("/:id", async (req, res) => {
+
+    try {
+        const foundProduct = await Product.findByIdAndUpdate({ _id: req.params.id }, req.body)
+        if (!foundProduct) {
+            throw new Error();
         }
-    })
-    product.save().then(result => {
-        console.log(result)
-    })
-        .catch(err => console.log(err))
-    res.status(201).json({
-        message: "product was created",
-        createdProduct: product
-    })
-})
-
-// retrieve product and it's price by id
-
-router.get("/:id", (req, res) => {
-    const id = req.params.id
-    //  call to api for product details
-    axios.get(`https://redsky.target.com/v2/pdp/tcin/${id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics`)
-        .then(apiProduct => {
-            const title = apiProduct.data.product.item.product_description.title;
-            // call to db model to retrieve product price
-            return title
-        }).then((title) => {
-            Product.findById(id).then(dbProduct => {
-                res.status(200).json({
-                    id: parseInt(dbProduct._id),
-                    name: title,
-                    current_price: dbProduct.current_price
-                })
-            })
-        }).catch(err => {
-            res.status(500).json({ error: err })
+        updatedPrice = await Product.findOne({ _id: req.params.id })
+        res.status(200).json({
+            message: "product has been updated",
+            updated_price: updatedPrice
         })
-})
-
-// update product value in database
-router.put("/:id", (req, res) => {
-    // find and update price in db
-    Product.findByIdAndUpdate({ _id: req.params.id }, req.body)
-        .then(() => {
-            //  request product from db again to return updated price
-            Product.findOne({ _id: req.params.id }).then(updatedPrice => {
-                res.send(updatedPrice)
-            })
+    } catch (err) {
+        res.status(404).json({
+            message: "no product found"
         })
-        .catch(err => {
-            console.log(err)
-        })
+    }
 })
 
 module.exports = router
