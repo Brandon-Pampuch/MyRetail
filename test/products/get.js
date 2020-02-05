27b4c85a2e2dcf19261df6812b696402c3609a3f
Brandon_Pampuch <brandonpampuch@gmail.com>
2020-02-05 13:50:08 -0600

    add mongo-memory-server to production dependenceies

--- test/products/get.js
+++ test/products/get.js
@@ -7,3 +7,45 @@ const router = require('../../routers/products-router.js')
 const server = require('../../server')
 const DB = require('../../models')
 
+describe('Get /products', () => {
+    before((done) => {
+        DB.connectDB()
+            .then(() => done())
+            .catch((err) => done(err))
+    })
+    after((done) => {
+        DB.disconnectDB()
+            .then(() => done())
+            .catch((err) => done(err))
+
+    })
+    it('OK, creating product works', (done) => {
+        request(server.use(router)).post('/')
+            .send({
+                id: "87654321",
+                currency_code: "USD",
+                value: 1.99
+            })
+            .then((res) => {
+                const body = res.body
+                console.log('product', body)
+                expect(body).to.contain.property('createdProduct')
+                done()
+            })
+            .catch((err) => done(err))
+    })
+
+    it('Fail, creating product requires id', (done) => {
+        request(server.use(router)).post('/')
+            .send({
+                currency_code: "USD",
+                value: 1.99
+            })
+            .then((res) => {
+                const body = res.body
+                expect(body).to.contain.property('error')
+                done()
+            })
+            .catch((err) => done(err))
+    })
+})
